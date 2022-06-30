ad4cc7fd291f64af3a3e5892376870034e58df6b
Daniel Afanador <danielthemisfits@gmail.com>
2022-06-30 17:19:34 -0300

    Programa para calcular raíz cuadrada

---
+++ squares.py
@@ -0,0 +1,10 @@
+def squared (a_number):
+    squared_number = a_number * a_number
+    return squared_number
+
+squared_10 = squared (10)
+squared_16 = squared (16)
+
+print (squared_10)
+print (squared_16)
+
